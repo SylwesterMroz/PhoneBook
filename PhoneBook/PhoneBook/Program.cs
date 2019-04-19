ee903a31210d222cd4ebbbe3d3c59387cc4f9da6
Sylwester Mroz <sylwester.mroz@globallogic.com>
2019-04-19 15:54:18 +0200

    Fixed some bugs

--- PhoneBook/PhoneBook/Program.cs
+++ PhoneBook/PhoneBook/Program.cs
@@ -11,7 +11,8 @@
             SeedPhoneBookWithData();
             Console.WriteLine("Phone book");
             Console.WriteLine("-----------------------");
-            Console.WriteLine("Find by: Location, Name, ID \nor Add new employee \nor Quit");
+            Console.WriteLine("Find by: Location, Name, ID \nAdd new employee: type Add and then Name, Surname, BadgeID, Location, Internal Phone with spaces in between" +
+                " \nor Quit to quit");
             string textFromLine;
             while (true)
             {
@@ -20,25 +21,74 @@
                 switch (commands[0])
                 {
                     case "Location":
-                        Console.WriteLine(AllEmployeesInLocation(commands[1]));
-                            break;
+                        try
+                        {
+                            Console.WriteLine(AllEmployeesInLocation(commands[1]));
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("You did not put anything");
+                        }
+                        break;
                     case "Name":
-                        Console.WriteLine(FindEmployeesByName(commands[1]));
+                        try
+                        {
+                            Console.WriteLine(FindEmployeesByName(commands[1]));
+                        }
+                        catch(IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("You did not put anything");
+                        }
+                        
                         break;
                     case "ID":
-                        int findID = Convert.ToInt32(commands[1]);
-                        Console.WriteLine(FindEmployeesByID(findID));
+                        try
+                        {
+                            int findID = Convert.ToInt32(commands[1]);
+                            Console.WriteLine(FindEmployeesByID(findID));
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("BadgeID value accepts only numbers");
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("You did not put anything");
+                        }
+                        
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("BadgeID too long");
+                        }
+
                         break;
                     case "Add":
-                        int id = Convert.ToInt32(commands[3]);
-                        Locations loc;
-                        Enum.TryParse(commands[4], out loc);
-                        AddNewEmployee(commands[1], commands[2], id, loc, commands[5]);
+                        try
+                        {
+                            int id = Convert.ToInt32(commands[3]);
+                            Locations loc;
+                            Enum.TryParse(commands[4], out loc);
+                            AddNewEmployee(commands[1], commands[2], id, loc, commands[5]);
+                            
+                        }
+                        catch(FormatException)
+                        {
+                            Console.WriteLine("Some value was incorrect");
+                        }
+                        catch(IndexOutOfRangeException)
+                        {
+                            Console.WriteLine("You didn't put enough data");
+                        }
+                        catch (OverflowException)
+                        {
+                            Console.WriteLine("BadgeID too long");
+                        }
                         break;
+
                     case "Quit":
                         return;
                     default:
-                        Console.WriteLine("Unknown value");
+                        Console.WriteLine("Unknown value. \nValid commands: Location, Name, ID, Add, Quit");
                         break;
                 }
             }
@@ -78,6 +128,7 @@
 
         private static void AddNewEmployee(string name, string surName, int badgeID, Locations department, string internalPhone)
         {
+
             var employee5 = new Employee(name, surName, badgeID, department, internalPhone);
             myPhoneBook.AddEmployee(employee5);
             Console.WriteLine(employee5.PrintFullInfo());
